e249fca3ade30e104472887655dd8ecba91653e9
Tim Sakow <tcsakow@gmail.com>
2019-12-09 13:11:18 -0500

    First Laravel commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+    @extends('layout')
+
+    @section('content')
+    <h1>Contact Information</h1>
+    <p>Contacts Here</p>
+    @endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+    @extends('layout')
+
+    @section('home')
+    <h1>Welcome to Laravel</h1>
+    <p>This is the content of the page</p>
+    @endsection
