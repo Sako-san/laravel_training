93a1a2da8a3d6402cb0ffe7f0d91e5c3c73539fe
Tim Sakow <tcsakow@gmail.com>
2019-12-09 14:05:13 -0500

    Named routes and generating URLs

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,7 +7,11 @@
     <title>Document</title>
 </head>
 <body>
-    @yield('home')
+    <ul>
+        <li><a href="{{ route('home') }}">Home</a></li>
+        <li><a href="{{ route('contact') }}">Contact</a></li>
+        <li><a href="{{ route('blog-post', ['id' => 1]) }}">Blog Post 1</a></li>
+    </ul>
     @yield('content')
 </body>
 </html>
