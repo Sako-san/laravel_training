9c5a9cde88b94a28f446b51d165e3eb0632bc239
Tim Sakow <tcsakow@gmail.com>
2019-12-10 13:20:25 -0500

    Created update functionality to the app S3_L27

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -74,8 +74,17 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(StorePost $request, $id)
     {
+        $post = BlogPost::findOrFail($id);
+        $validatedData = $request->validated();
+
+        $post->fill($validatedData);
+        $post->save();
+        $request->session()->flash('status', 'Blog post was updated!');
+
+        return redirect()->route('posts.show', ['post' => $post->id]);
+
     }
 
     // /**
